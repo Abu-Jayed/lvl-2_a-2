9001e4189854e92807793ae77700685de920c211
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-25 20:44:27 +0600

    user controller created.

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express'
+import User from '../models/user.model'
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body
+
+    const result = await User.create(userData)
+    res.status(201).json({
+      status: 'success',
+      message: 'User created successfully',
+      data: result,
+    })
+  } catch (error: any) {
+    console.log(error)
+    res.status(500).json({
+      status: 'fail',
+      message: error.message || 'something went wrong',
+    })
+  }
+}
+
+export const userController = {
+  createUser,
+}
