7b590d6a52b577f005c7b30ab03694b49ac443ac
Abu Jayed <aj.webdevlopment@gmail.com>
2023-11-25 17:56:49 +0600

    eslint configure and initial set up done.

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from './app'
+import mongoose from 'mongoose'
+
+const port = 5000
+
+// getting-started.js
+
+async function server() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    console.log("connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+server().catch((err) => console.log(err))
