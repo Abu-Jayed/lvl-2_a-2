3d7cacb3eeb9a60d4f4de45a8f196cecee534c1a
Abu Jayed <aj.webdevlopment@gmail.com>
2023-12-14 20:58:59 +0600

    dply err.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import express, { Request, Response } from 'express'
 import { userRoutes } from './routes/user.route'
 import cors from "cors"
+import User from './models/user.model'
 
 const app = express()
 
@@ -14,6 +17,23 @@ app.get('/', (req: Request, res: Response) => {
     status: 'success',
     message: 'Successfully server running',
   })
+})
+
+app.get("/api/users", async (req: Request, res: Response) => {
+  try {
+    const result = await User.find()
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    })
+  } catch (error: any) {
+    console.log(error)
+    res.status(500).json({
+      status: 'fail',
+      message: error.message || 'something went wrong',
+    })
+  }
 })
 
 
